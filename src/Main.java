98791e18d83e04bd029cba6e2ab559f6904b5df9
reviracio <mic.blaszczyk@gmail.com>
2018-05-03 08:38:55 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String Args[]){
+        int range=100;
+        for(int i=0; i<=100; i++){
+            if(i%3==0){
+                System.out.print("Fizz");
+            }
+            if(i%5==0){
+                System.out.print("Buzz");
+            }
+            if(i%3!=0 && i%5!=0){
+                System.out.print(i);
+            }
+            System.out.println();
+
+        }
+    }
+}
